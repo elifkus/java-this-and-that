28a6049258115348ffadedeb7d4a30f5780939ac
Elif T. Kus <elifkus@gmail.com>
2015-11-09 13:37:32 +0200

    Added faster solution

--- Problems/src/com/safkanyazilim/anagram/Solution.java
+++ Problems/src/com/safkanyazilim/anagram/Solution.java
@@ -63,4 +63,34 @@ public class Solution {
 		return diffCount;
 	}
 	
+	public static int findDifferenceToMakeAnagramFaster(String concatenatedString) {
+		if ((concatenatedString == null) || (( concatenatedString.length() % 2) != 0)) {
+			return -1;
+		}
+		
+		int half = concatenatedString.length() / 2;
+		char[] first = concatenatedString.substring(0, half).toCharArray();
+		char[] second = concatenatedString.substring(half).toCharArray();
+		
+		int charTypeNumber = 26;
+		
+		int[] firstCharsCount = new int[charTypeNumber];
+		int[] secondCharsCount = new int[charTypeNumber];
+		
+		for(int i=0; i<half; i++) {
+			firstCharsCount[first[i] - 'a'] += 1;
+			secondCharsCount[second[i] - 'a'] += 1;
+		}
+		
+		int diffCount = 0;
+		
+		for(int i=0; i<charTypeNumber; i++) {
+			if (firstCharsCount[i] != secondCharsCount[i]) {
+				diffCount++;
+			}
+		}
+		
+		return diffCount / 2;
+	}
+	
 }
---
+++ Problems/test/com/safkanyazilim/anagram/SolutionTest.java
@@ -0,0 +1,93 @@
+package com.safkanyazilim.anagram;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SolutionTest {
+
+	@Test
+	public void testfindDifferenceToMakeAnagram() {
+		String item = "aabbabab";
+		int expected = 0;
+		
+		assertEquals(expected, Solution.findDifferenceToMakeAnagram(item));
+	}
+
+	@Test
+	public void testSpeedForFindDifferenceToMakeAnagram() {
+		String[] items = {"aabbabab",
+						  "aba",
+						  "sdasdfasdf",
+						  "yhohksdf",
+						  "hjhjhhjj",
+						  "asdfafff",
+						  "ijkjkkkk",
+						  "asdffdsa",
+						  "akkaakka",
+						  "kitapcinin",
+						  "aabbcbab",
+						  "aba",
+						  "sdasddasds",
+						  "yjohmsdf",
+						  "hjljhljj",
+						  "asdfaaff",
+						  "ijkjkakk",
+						  "aslfflsa",
+						  "fknafkna",
+						  "kitaplinin"};
+		
+		long start = System.currentTimeMillis();
+		
+		for(int j=0; j<1000000; j++) {
+			for (int i = 0; i<20; i++) {
+				Solution.findDifferenceToMakeAnagram(items[i]);
+			}
+		}
+		
+		long end = System.currentTimeMillis();
+		
+		long elapsedTime = end-start;
+		
+		System.out.println("Total time elapsed 1: "+ String.valueOf(elapsedTime));
+		
+	}
+
+	@Test
+	public void testfindDifferenceToMakeAnagramFaster() {
+		String item = "aabbabab";
+		int expected = 0;
+		
+		assertEquals(expected, Solution.findDifferenceToMakeAnagramFaster(item));
+	}
+
+	@Test
+	public void testSpeedForFindDifferenceToMakeAnagramFaster() {
+		String[] items = {"aabbabab",
+						  "aba",
+						  "sdasdfasdf",
+						  "yhohksdf",
+						  "hjhjhhjj",
+						  "asdfafff",
+						  "ijkjkkkk",
+						  "asdffdsa",
+						  "akkaakka",
+						  "kitapcinin"};
+		
+		long start = System.currentTimeMillis();
+		
+		for(int j=0; j<1000000; j++) {
+			for (int i = 0; i<10; i++) {
+				Solution.findDifferenceToMakeAnagramFaster(items[i]);
+			}
+		}
+		
+		long end = System.currentTimeMillis();
+		
+		long elapsedTime = end-start;
+		
+		System.out.println("Total time elapsed 2: "+ String.valueOf(elapsedTime));
+		
+	}
+
+}
