9b3ac571fc996ab1b143ed03c77d0f36c2058da1
Elif T. Kus <elifkus@gmail.com>
2016-03-14 14:56:44 +0200

    Improved code for searchmatrix

--- Problems/src/com/safkanyazilim/matrixsearch/Solution.java
+++ Problems/src/com/safkanyazilim/matrixsearch/Solution.java
@@ -5,37 +5,40 @@ import java.util.ArrayList;
 
 public class Solution {
 	public static int searchMatrix(ArrayList<ArrayList<Integer>> a, int b) {
-	    int n = a.size();
-	    
-	    if (n < 1) {
-	        return 0;
-	    }
-	    
-	    int m = a.get(0).size();
-	    
-	    int totalNumbers = m * n;
-	
-	    int start = 0;
-	    int end = totalNumbers;
-	    int current = (end + start)/2;
-	    int midRow = current / m;
-	    int midColumn = current % m;
-	    
-	    while ( start < end -1) {
-	        if (b == a.get(midRow).get(midColumn)) {
-	            return 1;
-	        } else if (b > a.get(midRow).get(midColumn)) {
-	            start = current;
-	        } else {
-	            end = current;
-	        }
-	        
-	        current = (end + start)/2;
-	        midRow = current / m;
-	        midColumn = current % m;
-	    }
-	    
-	    return 0;
+		 int n = a.size();
+		    
+		    if (n < 1) {
+		        return 0;
+		    }
+		    
+		    int m = a.get(0).size();
+		    
+		    int totalNumbers = m * n;
+		
+		    int start = 0;
+		    int end = totalNumbers-1;
+		    int current;
+		    int midRow;
+		    int midColumn;
+
+		    while ( start <= end ) {
+		        current = (end + start)/2;
+		        midRow = current / m;
+		        midColumn = current % m;
+		        
+		        if (b == a.get(midRow).get(midColumn)) {
+		            return 1;
+		        } else if (b > a.get(midRow).get(midColumn)) {
+		            start = current + 1;
+		        } else {
+		            end = current - 1;
+		        }
+		        
+		       
+
+		    }
+		    
+		    return 0;
 	}
 	
 	
