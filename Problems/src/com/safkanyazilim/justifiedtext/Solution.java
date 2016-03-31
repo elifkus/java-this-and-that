35b0d7fcf31e38c0293b2404e670ebfaa0933854
Elif T. Kus <elifkus@gmail.com>
2016-03-31 14:09:39 +0300

    Added code and text for justified text problem

---
+++ Problems/src/com/safkanyazilim/justifiedtext/Solution.java
@@ -0,0 +1,82 @@
+package com.safkanyazilim.justifiedtext;
+
+import java.util.ArrayList;
+
+/**
+ * Solution for <insert problem url>
+ * @author elif
+ *
+ */
+public class Solution {
+	public static ArrayList<String> fullJustify(ArrayList<String> a, int b) {
+		int lineStartWordIndex = 0;
+		int lineEndWordIndex = 0;
+		int currentWordIndex = 0;
+		
+		int currentLineLength=0;
+		
+		ArrayList<String> justifiedLines = new ArrayList<String>();
+		
+		while (currentWordIndex<a.size()) {
+			System.out.println("Outer loop");
+			System.out.println("Current: " + currentWordIndex);
+			while (currentWordIndex < a.size() && b >= a.get(currentWordIndex).length() + currentLineLength) {
+				lineEndWordIndex = currentWordIndex;
+				currentLineLength += a.get(currentWordIndex).length();
+				currentLineLength += 1;
+				currentWordIndex++;
+				System.out.println("First Inner loop");
+			}
+			currentLineLength--;
+			
+			String line = justifyWordsIntoLine(a, b, lineStartWordIndex, lineEndWordIndex, currentLineLength);
+			justifiedLines.add(line);
+			
+			currentLineLength = 0;
+			lineStartWordIndex = lineEndWordIndex+1;
+			
+		}
+		
+		
+		return justifiedLines;
+	}
+	
+	public static String justifyWordsIntoLine(ArrayList<String> a, int b, int startIndex, int endIndex, int currentLineLength) {
+		StringBuffer buffer = new StringBuffer();
+		
+		if (startIndex==endIndex) {
+			buffer.append(a.get(startIndex));
+			b = b - a.get(startIndex).length();
+			
+			while (b > 0) {
+				buffer.append(" ");
+				b--;
+			}
+		} else {
+			int spaces = b - currentLineLength;
+			int minimumSpace = 1 + (spaces / (endIndex - startIndex));
+			int extraSpaceForLine = spaces % (endIndex - startIndex);
+			
+			while(startIndex < endIndex ) {
+				buffer.append(a.get(startIndex));
+				
+				int counter = minimumSpace;
+				while(counter > 0) {
+					buffer.append(" ");
+					counter--;
+				}
+				
+				if (extraSpaceForLine >0) {
+					buffer.append(" ");
+					extraSpaceForLine--;
+				}
+				startIndex++;
+			}
+			
+			buffer.append(a.get(startIndex));
+		}
+	
+		return buffer.toString();
+	}
+
+}
---
+++ Problems/test/com/safkanyazilim/justifiedtext/SolutionTest.java
@@ -0,0 +1,44 @@
+package com.safkanyazilim.justifiedtext;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.junit.Test;
+
+public class SolutionTest {
+
+	@Test
+	public void testFullJustify1() {
+		ArrayList<String> input1 = new ArrayList<String>(Arrays.asList("This", "is", "an", "example", "of", "text", "justification."));
+		int input2 = 16;
+		ArrayList<String> expected = new ArrayList<String>(Arrays.asList("This    is    an", "example  of text", "justification.  "));
+		ArrayList<String> actual = Solution.fullJustify(input1, input2);
+		
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testFullJustify2() {
+		ArrayList<String> input1 = new ArrayList<String>(Arrays.asList("America"));
+		int input2 = 7;
+		ArrayList<String> expected = new ArrayList<String>(Arrays.asList("America"));
+		ArrayList<String> actual = Solution.fullJustify(input1, input2);
+		
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void testFullJustify3() {
+		ArrayList<String> input1 = new ArrayList<String>(Arrays.asList("dinosaurs"));
+		int input2 = 11;
+		ArrayList<String> expected = new ArrayList<String>(Arrays.asList("dinosaurs  "));
+		ArrayList<String> actual = Solution.fullJustify(input1, input2);
+		
+		assertEquals(expected, actual);
+	}
+	
+	
+
+}
