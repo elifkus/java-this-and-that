3c5daa2c0aa665b4dbf578c8e5fc0df98ffea4ec
Elif T. Kus <elifkus@gmail.com>
2016-08-15 16:10:04 +0300

    Added new problem solution and tests

---
+++ Problems/src/com/safkanyazilim/palindromepermutation/Solution.java
@@ -0,0 +1,60 @@
+package com.safkanyazilim.palindromepermutation;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class Solution {
+
+	public static boolean isPermutationOfAPalindrome1(String text) {
+		HashSet<Character> set = new HashSet<>();
+		
+		char[] textCharArray = text.toCharArray();
+		for (char c : textCharArray) {
+			if (c != ' ') {
+				if (set.contains(c)) {
+					set.remove(c);
+				} else {
+					set.add(c);
+				}
+			}
+		}
+		
+		boolean isPalindromePermutation = true;
+		
+		if (set.size() > 1) {
+			isPalindromePermutation = false;
+		} 
+		
+		return isPalindromePermutation;
+	}
+	
+    public static boolean isPermutationOfAPalindrome2(String text) {
+		
+		char[] textCharArray = text.toCharArray();
+		Arrays.sort(textCharArray);
+		
+		int index=0;
+		boolean midCharacterExists = false;
+		
+		while (index < (textCharArray.length -1)) {
+			if (textCharArray[index] == ' ') {
+				index++;
+			} else if (textCharArray[index] == textCharArray[index+1]) {
+				index += 2;
+			} else {
+				if (!midCharacterExists) {
+					midCharacterExists = true;
+					index++;
+				} else {
+					return false;
+				}
+			}
+		}
+		
+		if (midCharacterExists == true && index == (textCharArray.length-1)) {
+			return false;
+		}
+		
+		return true;
+	}
+}
---
+++ Problems/test/com/safkanyazilim/palindromepermutation/SolutionTest.java
@@ -0,0 +1,163 @@
+package com.safkanyazilim.palindromepermutation;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SolutionTest {
+
+	@Test
+	public void testIsPermuationOfAPalindrome1_1() {
+		String item1 = "aabbabab";
+		
+		boolean expected = true;
+		
+		assertEquals(expected, Solution.isPermutationOfAPalindrome1(item1));
+	}
+	
+	@Test
+	public void testIsPermuationOfAPalindrome1_2() {
+		String item1 = "aabbcdabab";
+		
+		boolean expected = false;
+		
+		assertEquals(expected, Solution.isPermutationOfAPalindrome1(item1));
+	}
+	
+	@Test
+	public void testIsPermuationOfAPalindrome1_3() {
+		String item1 = "cat of o cat";
+		
+		boolean expected = true;
+		
+		assertEquals(expected, Solution.isPermutationOfAPalindrome1(item1));
+	}
+	
+	@Test
+	public void testIsPermuationOfAPalindrome1_4() {
+		String item1 = "abbabbg";
+		
+		boolean expected = true;
+		
+		assertEquals(expected, Solution.isPermutationOfAPalindrome1(item1));
+	}
+
+	//@Test
+	public void testSpeedForIsPermuationOfAPalindrome1() {
+		String[] items = {"aabbabab",
+						  "aba",
+						  "sdasdfasdf",
+						  "yhohksdf",
+						  "hjhjhhjj",
+						  "asdfafff",
+						  "ijkjkkkk",
+						  "asdffdsa",
+						  "akkaakka",
+						  "kitapcinin",
+						  "aabbcbab",
+						  "aba",
+						  "sdasddasds",
+						  "yjohmsdf",
+						  "hjljhljj",
+						  "asdfaaff",
+						  "ijkjkakk",
+						  "aslfflsa",
+						  "fknafkna",
+						  "kitaplinin"};
+		
+		long start = System.currentTimeMillis();
+		
+		for(int j=0; j<10000; j++) {
+			for (int i = 0; i<20; i++) {
+				boolean isPermutation = Solution.isPermutationOfAPalindrome1(items[i]);
+				System.out.print(items[i] + ":" + isPermutation + "|");
+			}
+		}
+		System.out.println("");
+		long end = System.currentTimeMillis();
+		
+		long elapsedTime = end-start;
+		
+		System.out.println("Total time elapsed 1: "+ String.valueOf(elapsedTime));
+		
+	}
+	
+	@Test
+	public void testIsPermuationOfAPalindrome2_1() {
+		String item1 = "aabbabab";
+		
+		boolean expected = true;
+		
+		assertEquals(expected, Solution.isPermutationOfAPalindrome2(item1));
+	}
+	
+	@Test
+	public void testIsPermuationOfAPalindrome2_2() {
+		String item1 = "aabbcdabab";
+		
+		boolean expected = false;
+		
+		assertEquals(expected, Solution.isPermutationOfAPalindrome2(item1));
+	}
+	
+	@Test
+	public void testIsPermuationOfAPalindrome2_3() {
+		String item1 = "cat of o cat";
+		
+		boolean expected = true;
+		
+		assertEquals(expected, Solution.isPermutationOfAPalindrome2(item1));
+	}
+	
+	@Test
+	public void testIsPermuationOfAPalindrome2_4() {
+		String item1 = "abbabbg";
+		
+		boolean expected = true;
+		
+		assertEquals(expected, Solution.isPermutationOfAPalindrome2(item1));
+	}
+
+	//@Test
+	public void testSpeedForIsPermuationOfAPalindrome2() {
+		String[] items = {"aabbabab",
+						  "aba",
+						  "sdasdfasdf",
+						  "yhohksdf",
+						  "hjhjhhjj",
+						  "asdfafff",
+						  "ijkjkkkk",
+						  "asdffdsa",
+						  "akkaakka",
+						  "kitapcinin",
+						  "aabbcbab",
+						  "aba",
+						  "sdasddasds",
+						  "yjohmsdf",
+						  "hjljhljj",
+						  "asdfaaff",
+						  "ijkjkakk",
+						  "aslfflsa",
+						  "fknafkna",
+						  "kitaplinin"};
+		
+		long start = System.currentTimeMillis();
+
+		for(int j=0; j<10000; j++) {
+			for (int i = 0; i<20; i++) {
+				boolean isPermutation = Solution.isPermutationOfAPalindrome2(items[i]);
+				System.out.print(items[i] + ":" + isPermutation + "|");
+			}
+		}
+		System.out.println("");
+		long end = System.currentTimeMillis();
+		
+		long elapsedTime = end-start;
+		
+		System.out.println("Total time elapsed 1: "+ String.valueOf(elapsedTime));
+		
+	}
+	
+	
+
+}
