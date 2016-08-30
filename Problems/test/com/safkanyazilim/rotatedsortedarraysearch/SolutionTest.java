ea02d072806ed25f3799d64a7192d8172080cd38
Elif T. Kus <elifkus@gmail.com>
2016-08-30 15:59:39 +0300

    Added test for solution

--- Problems/test/com/safkanyazilim/rotatedsortedarraysearch/SolutionTest.java
+++ Problems/test/com/safkanyazilim/rotatedsortedarraysearch/SolutionTest.java
@@ -2,18 +2,21 @@ package com.safkanyazilim.rotatedsortedarraysearch;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.Test;
 
 public class SolutionTest {
 
 	@Test
-	public void testSearch() {
-		
-		//assertEquals(expected, Solution.findCommonLetter(item1, item2));
+	public void testFindIndexOfIntegerInRotatedArray() {
+		List<Integer> list = new ArrayList<Integer>(Arrays.asList(5, 6, 8, 10, 11, 13, 1, 3, 4));
+		int number = 3;
+		int expected = 7;
+		int actual = Solution.search(list, number);
+		assertEquals(expected, actual);
 	}
-
-	
-	
-	
 
 }
