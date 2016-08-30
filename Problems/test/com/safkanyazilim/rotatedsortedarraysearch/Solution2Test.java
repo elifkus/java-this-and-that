907d65756fd165421290cbe39027199907dc5b70
Elif T. Kus <elifkus@gmail.com>
2016-08-30 16:13:31 +0300

    Added time test for rotated array integer search problem

--- Problems/test/com/safkanyazilim/rotatedsortedarraysearch/Solution2Test.java
+++ Problems/test/com/safkanyazilim/rotatedsortedarraysearch/Solution2Test.java
@@ -15,7 +15,24 @@ public class Solution2Test {
 		assertEquals(expected, actual);
 	}
 
-	
+	@Test
+	public void testSpeedForFindIndexOfIntegerInRotatedArray() {
+		int[] array = {5, 6, 8, 10, 11, 13, 1, 3, 4};
+		int number = 3;
+		int expected = 7;
+
+		long start = System.currentTimeMillis();
+
+		for (int i=0; i<1000000; i++) {
+			int actual = Solution2.findIndexOfIntegerInRotatedArray(array, number);
+			assertEquals(expected, actual);
+		}
+		
+		long end = System.currentTimeMillis();
+		
+		long elapsedTime = end-start;
+		System.out.println("Total time elapsed 1: "+ String.valueOf(elapsedTime));
+	}
 	
 	
 
