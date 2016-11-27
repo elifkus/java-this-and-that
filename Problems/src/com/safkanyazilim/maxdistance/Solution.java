4324f3153688ab3653d420a185bd9ade89eedf11
Elif T. Kuş <elifkus@gmail.com>
2016-11-27 14:07:14 +0200

    Added new problems and tests

---
+++ Problems/src/com/safkanyazilim/maxdistance/Solution.java
@@ -0,0 +1,20 @@
+package com.safkanyazilim.maxdistance;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Solution for <insert problem url>
+ * @author elif
+ *
+ */
+public class Solution {
+	public static int maximumGap(final List<Integer> a) {
+		
+		
+		return 0;
+	}
+
+	
+}
---
+++ Problems/src/com/safkanyazilim/palindromelinkedlist/Solution.java
@@ -0,0 +1,27 @@
+package com.safkanyazilim.palindromelinkedlist;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+/**
+ * Solution for Palindrome: Implement a function to check if a linked list is a palindrome.
+ * @author elif
+ *
+ */
+public class Solution {
+	public static boolean isPalindrome (LinkedList<Character> string) {
+			
+		try (Scanner scanner = new Scanner(System.in)) { 
+			int numberOfTestCases = scanner.nextInt();
+			
+			for(int k=0; k<numberOfTestCases; k++) {
+				//String str1 = scanner.next();
+				
+			}
+	
+		}		
+		
+		return false;
+	}	
+
+}
