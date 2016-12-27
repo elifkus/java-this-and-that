de07478f31c94380bc8e0a77210422e548434465
Elif T. Kuş <elifkus@gmail.com>
2016-12-27 14:35:18 +0200

    Added polycarp decoding question boilerplate code

---
+++ Problems/src/com/safkanyazilim/polycarpdecoding/Solution.java
@@ -0,0 +1,25 @@
+package com.safkanyazilim.polycarpdecoding;
+
+import java.util.Scanner;
+
+/**
+ * Solution for http://codeforces.com/problemset/problem/746/B
+ * @author elif
+ *
+ */
+public class Solution {
+	public static void main(String[] args) {
+			
+		try (Scanner scanner = new Scanner(System.in)) { 
+			int numberOfTestCases = scanner.nextInt();
+			
+			for(int k=0; k<numberOfTestCases; k++) {
+				//String str1 = scanner.next();
+				
+			}
+	
+		}		
+		
+	}	
+
+}
